361897c8a490e8716a45c46438e83d1f27ada952
K Shuvo Bakar <shuvo.bakar@gmail.com>
2026-07-26 21:12:49 +1000

    Initial commit

--- R/s3_fnc.R
+++ R/s3_fnc.R
@@ -2,8 +2,7 @@
 print.svyLocAdj <- function(x, ...){
  cat("Survey spatial location displacement model\n")
  cat(paste0("Model family: ",x$results$family,"\n"))
- waic <- data.frame(lppd = x$results$lppd,
-                    waic_approximation = x$results$waic_approx)
+ waic <- data.frame(waic_approximation = x$results$waic_approx)
  row.names(waic) <- c("Est.")
  print(x$results$fixed_parameters)
  cat(paste0("Watanabe-Akaike information criterion: \n"))
@@ -135,8 +134,7 @@ summary.svyLocAdj <- function(object, ...){
   cat(paste0("Model variability parameters\n"))
   print(object$results$variability_parameters)
   cat(paste0("----------------------------\n"))
-  waic <- data.frame(lppd = object$results$lppd,
-          waic_approximation = object$results$waic_approx)
+  waic <- data.frame(waic_approximation = object$results$waic_approx)
   row.names(waic) <- c("Est.")
   cat(paste0("Watanabe–Akaike information criterion \n"))
   print(t(waic))
